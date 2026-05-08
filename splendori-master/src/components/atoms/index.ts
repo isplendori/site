bf8e13e3e643978b23a14915c0bf25f8958fd67f
Anna Ishii <fromishou@gmail.com>
2026-05-08 17:37:42 -0300

    Delete splendori-master directory

--- splendori-master/src/components/atoms/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as Button } from "./Button";
-export { default as Input } from "./Input";
-export { default as Label } from "./Label";
-export { default as NavLink } from "./NavLink";
-export { default as Logo } from "./Logo";
-export { default as DropdownItem } from "./DropdownItem";
-export { default as FloatingMessage } from "./FloatingMessage";
-export { default as HeroArchitecture } from "./HeroArchitecture";
-export * from "./Icon";
